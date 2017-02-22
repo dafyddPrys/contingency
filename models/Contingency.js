670d9e09917247e2dfdd8b00374170d78818a82f
Dafydd Thomas <jingovimes@gmail.com>
2017-02-22 22:36:16 +0000

    add properties tand methods to schema

--- models/Contingency.js
+++ models/Contingency.js
@@ -1,33 +1,93 @@
 
 const mongoose = require('mongoose');
+const moment = require('moment');
 
+// const frequencyEnum = {
+//   day: 'day',
+//   week: 'week',
+//   'second-week': 'second-week',
+//   month: 'month',
+//   year: 'year'
+// };
 
-const contingencyTypeEnum = {
-  email: 'email',
-  webhook: 'webhook'
-};
-
-const activeStatusEnum = {
-  active: 'active',
-  paused: 'paused'
-};
 
 const contingencySchema = new mongoose.Schema({
-  status: {
+  type: 'webhook',
+  isEnabled: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  checkedIn: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  frequency: { // the number of milliseconds in the frequency.
+    type: Number,
+    required: true
+  },
+  fromTime: {
+    type: Date,
+    required: true
+  },
+  nextTriggerTime: {
+    type: Date,
+    required: true,
+  },
+  webhookUrl: {
     type: String,
-    default: 'paused',
+    required: true,
+    // TODO custom validator here for url regex
+  },
+  webhookBody: {
+    type: {},
+    required: true
   }
+
 });
 
 
 /**
- * Schema methods
+ * Validation methods
  * ------------------------------------------------------------------
  */
 
-// contingencySchema.methods.activate = function activate() {
 
-// }
+/**
+ * Instance methods
+ * ------------------------------------------------------------------
+ */
+
+/**
+ * Move a next trigger time onto the next trigger time
+ */
+contingencySchema.methods.tickNextTriggerTime = function tickNextTriggerTime() {
+  // if the nextTriggerTime is in the past, move it on and save the contingency.
+  // return a promise of the save.
+  if (moment(this.nextTriggerTime) < moment()) {
+    this.nextTriggerTime = moment(this.nextTriggerTime).add(this.frequency, 'ms');
+    return this.save.exec();
+  }
+  // otherwise, return a n empty resolved promise.
+  return Promise.resolve();
+};
+
+/**
+ * Static methods
+ * ------------------------------------------------------------------
+ */
+
+/**
+ * Get all contingencies which have the given time as their next trigger time.
+ */
+contingencySchema.statics.findTriggersForTime = function findTriggersForTime(time) {
+  return this
+    .find({
+      nextTriggerTime: time
+    })
+    .exec();
+};
 
 
 /**
